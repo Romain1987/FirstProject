39949031219d1820cd7c70aa64a13a99f80fcf8b
Romain <romain1987@outlook.fr>
2016-06-06 22:40:18 +0200

    commit via intellij

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,7 @@ public class Test {
 
     public static void main(String [] args){
         System.out.println("First Git Commit");
+        System.out.println("Commit branch");
         System.out.println("test ok");
     }
 
