a1605ffeeecdf62e37a25b3c956656934a89207c
Romain <romain1987@outlook.fr>
2016-06-12 10:38:23 +0200

    ff

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String [] args){
         System.out.println("First Git Commit");
         System.out.println("test ok");
-
+        System.out.println("test ok");
 
     }
 
